a2f17e58137e11b133912cb25b3e4ab31ece1537
Jordan Duggan <Jordan.Duggan@Gmail.com>
2016-05-28 21:31:26 -0400

    Added vacation log to team

--- lib/jira-profiler/team.rb
+++ lib/jira-profiler/team.rb
@@ -16,4 +16,19 @@ module JiraProfiler
 
   end
 
+  # Record of when people were on vacation
+  def vacation_log(log_filepath)
+    @vacation_log = {}
+    data = CSV.read(log_filepath, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all})
+    data.each do |r|
+      d = Date.new(r[:year], r[:month], r[:day])
+      k = d.strftime('%Y.%m.%d')
+      @vacation_log[r[:who]] = {} unless @vaction_log.has_key?(r[:who])
+      @vacation_log[k] = [] unless @vaction_log.has_key?(k)
+      @vacation_log[k] << r[:who]
+    end
+
+    @vacation_log
+  end
+
 end
